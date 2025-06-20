249f557236be48650ce9b0cfedcd4f631ad3d1f7
Alicia Portela <alicia.portela@alumnos.upm.es>
2025-06-20 12:29:35 +0200

    Update plot_PCA.R

--- 1. Genomic data/plot_PCA.R
+++ 1. Genomic data/plot_PCA.R
@@ -4,9 +4,11 @@ library(ggplot2)
 pca_data <- read.table("PCA_SNPs_under_sel_pop.txt", header=TRUE, stringsAsFactors = FALSE)
 
 # Graficar PC1 vs PC2 por población
-ggplot(pca_data, aes(x=PC1, y=PC2, color=POPULATION)) +
+plot<- ggplot(pca_pop, aes(x=PC1, y=PC2, color=CONTINENT)) +
   geom_point(size=3) +
   theme_minimal() +
-  labs(title="PCA all data", x="PC1", y="PC2") +
+  labs(title="PCA_all", x="PC1", y="PC2") +
   theme(legend.title = element_text(size=12),
-        legend.text = element_text(size=10))
+        legend.text = element_text(size=10)) + stat_ellipse()
+
+ggsave("PCA_all.png", plot = plot, width = 12, height = 10, dpi = 300)
