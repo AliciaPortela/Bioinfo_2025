6a9c40afeac0be51a39387ca4e7ff11830221e94
Alicia Portela <alicia.portela@alumnos.upm.es>
2025-06-22 19:55:19 +0200

    Update exctract_SNPs_vcf.sh

--- 2. GRoSS/Outputs/exctract_SNPs_vcf.sh
+++ 2. GRoSS/Outputs/exctract_SNPs_vcf.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# compress and index vcf file
+bgzip -c hgdp_tgp_sgdp_chr12_p.dated_287.vcf > file.vcf.gz
+tabix -p vcf hgdp_tgp_sgdp_chr12_p.dated_287.vcf.gz
 
 ./bcftools/bcftools view -R SNPs_under_sel_3.txt -o ./human_data_SNPs_under_sel.vcf -O v ./hgdp_tgp_sgdp_chr12_p.dated_287.vcf.gz
