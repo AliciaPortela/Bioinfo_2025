728b1814a69d80ae44f20fe195dd8180a54f77ce
Alicia Portela <alicia.portela@alumnos.upm.es>
2025-06-22 19:37:58 +0200

    Update findSel.sh

--- 3. Flink/Outputs/findSel.sh
+++ 3. Flink/Outputs/findSel.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # run findSel.R on bash 
+# run it for each group (and hierarchy) and each type of selection
 
-chr=""
-group=""
+chr="chr12" 
+group="" # name of the group
 index=0 # change to group index from 0-6
 sel="" # type of selection: divergent or balancing
-prob=0.99
+prob=0.99999
 
 Rscript findSel.R $chr $group $index $sel $prob
