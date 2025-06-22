4e86a29086436051bc9583d5371079b9b91978cb
Alicia Portela <alicia.portela@alumnos.upm.es>
2025-06-22 20:18:15 +0200

    Update join_SNPs.sh

--- 3. Flink/Outputs/join_SNPs.sh
+++ 3. Flink/Outputs/join_SNPs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # join all SNPs under balancing selection to filter original VCF with bcftools
-awk '{print $2 "\t" $4}' coefSelbalancing* | sort -k2,2n | uniq -f1 > all_balanced_loci.txt
+awk '{print "1\t" $4}' coefSelbalancing* | sort -k2,2n | uniq -f1 > all_balanced_loci.txt
 
 # join all SNPs under divergent selection to filter original VCF with bcftools
-awk '{print $2 "\t" $4}' coefSeldivergent* | sort -k2,2n | uniq -f1 > all_divergent_loci.txt
+awk '{print "1\t" $4}' coefSeldivergent* | sort -k2,2n | uniq -f1 > all_divergent_loci.txt
